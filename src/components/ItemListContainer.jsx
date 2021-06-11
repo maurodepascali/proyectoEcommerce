735e7dfb7a85405fa53692b9f5e7a7bb6d3db634
maurodepascali <mauro.depascali@hotmail.com>
2021-06-11 11:26:21 -0300

    Add files via upload

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function ItemListContainer() {
+
+/*  const producto = [
+        {id:1, name:'torta oreo', price: 1500},
+        {id:2, name:'torta brownie', price: 1000},
+        {id:3, name:'alfajores', price: 500},
+        {id:4, name:'cheesecake', price: 2500}
+    ];
+*/ 
+  return (
+      
+    <div>
+        <h1>Torta Oreo</h1>
+        <h6>Precio: $1500</h6>
+        <h1>Torta Brownie</h1>
+        <h6>Precio: $1000</h6>
+    </div>
+  );
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,55 @@
 import React from 'react';
+
+
+import logo from "../imagenes/logo.png";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import logo from "../imagenes/logo.png";
 import { Badge } from '@material-ui/core';
 import { AccountCircle, ShoppingCart } from '@material-ui/icons';
-
 
 
 export default function Navbar(){
 
     return (
-        <div>
-            <AppBar position="fixed">
+        
+<div className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div className="container-fluid">
+    <a className="navbar-brand"><img src={logo} style={{width:"100px"}}/></a> 
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="justify-content-end" id="navbarColor01">
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <a className="nav-link active" href="#">Inicio
+            <span className="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Productos</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contacto</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Carrito</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+
+</div>
+
+      
+
+
+/*
+<AppBar position="fixed">
                 <Toolbar>
                     <IconButton>
                         <img src={logo} style={{width:"100px"}}/>
@@ -46,8 +81,44 @@ export default function Navbar(){
                 </Toolbar>
             </AppBar>
 
-        </div>
+*/ 
+
+/*
+<div className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Nonna Julia</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="justify-content-end" id="navbarColor01">
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <a className="nav-link active" href="#">Inicio
+            <span className="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Productos</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contacto</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Carrito</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+
+</div>
 
 
+
+
+
+
+*/ 
     );
 }
