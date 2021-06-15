868070ed514b184d939fbc5bc3c5323d26a6c6c9
maurodepascali <mauro.depascali@hotmail.com>
2021-06-14 21:40:01 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/NavBar';
-import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ItemListContainer from './components/ItemListContainer';
+import {ItemListContainer} from './components/ItemListContainer.jsx';
+
+
 
 function App() {
   
     return (
       <div>
         
-      <Navbar/>
-      <ItemListContainer greeting={"Tortas"}/>
+     <ItemListContainer greeting="Soy una torta"/>
+      
+     
        
       </div>
     );
