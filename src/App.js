f2956204b2a030948a188be354e57d165970c9ca
maurodepascali <mauro.depascali@hotmail.com>
2021-06-18 13:52:50 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {ItemListContainer} from './components/ItemListContainer.jsx';
+import Nav from './components/Nav';
+import Products from './components/Products';
+import Product from './components/Product';
+import './styles/styles.css';
+import {NavBar} from './components/NavBar';
+import {Main} from './components/Main';
 
+
+//https://api.mercadolibre.com/sites/MLA/search#options
 
 
 function App() {
   
     return (
-      <div>
-        
-     <ItemListContainer greeting="Soy una torta"/>
       
-     
+      <div>
        
+        
+<Main/>
       </div>
+   
     );
   }
 export default App;
