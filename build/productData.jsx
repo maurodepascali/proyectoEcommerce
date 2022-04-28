5af02ddbd209d6e49ab8ff4564fde3fbbe0749bf
maurodepascali <mauro.depascali@hotmail.com>
2022-04-28 18:07:26 -0300

    Add files via upload

---
+++ build/productData.jsx
@@ -0,0 +1,34 @@
+const products = [
+    {
+      id: 1,
+      name: "TORTA OREO",
+      price: 1500,
+      stock: 5,
+      category: "torta",
+      description: "torta",
+      image:
+        "https://truffle-assets.imgix.net/f0f7a6e1-torta-oreo-y-ddl_l_thumb.jpg"
+    },
+    {
+        id: 2,
+        name: "TORTA OREO",
+        price: 1500,
+        stock: 5,
+        category: "torta",
+        description: "torta",
+        image:
+        "https://d1uz88p17r663j.cloudfront.net/resized/2017b5b6ae40076cad68b8d8fd489239_373_Cheesecake_sin_Horno_1200_600.jpg"
+    },
+    {
+        id: 3,
+        name: "TORTA OREO",
+        price: 1500,
+        stock: 5,
+        category: "torta",
+        description: "torta",
+        image:
+        "https://d1uz88p17r663j.cloudfront.net/resized/2017b5b6ae40076cad68b8d8fd489239_373_Cheesecake_sin_Horno_1200_600.jpg"
+      }
+
+    ]
+  
